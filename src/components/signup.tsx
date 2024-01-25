6e72406c3f7994a2c8b1a3bcd49e48da966606fd
Edward <edward.wo@wowow.io>
2024-01-25 21:14:13 +0800

    feat: add footer

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Checkbox, message, Form, Input, Select } from "antd";
+import { Button, Checkbox, message, Form, Input, Select, Divider } from "antd";
 import OtpInput from "react-otp-input";
 import { getCountryList } from "../requests";
 import { Country } from "../shared.types";
 
 import axios from "axios";
+import {
+  FacebookOutlined,
+  GithubOutlined,
+  LinkedinOutlined,
+  TwitterOutlined,
+} from "@ant-design/icons";
 const APP_PATH = import.meta.env.BASE_URL;
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -139,251 +145,298 @@ const Index = () => {
   }, []);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "100px",
-      }}
-    >
-      {currentStep == 0 ? (
-        <>
-          <h1 style={{ marginBottom: "36px" }}>Sign-up</h1>
-          <div
-            style={{
-              width: "640px",
-              border: "1px solid #e0e0e0",
-              borderRadius: "8px",
-              background: "#FFF",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              paddingTop: "24px",
-            }}
-          >
-            <Form
-              name="basic"
-              labelCol={{
-                span: 10,
-              }}
-              wrapperCol={{
-                span: 16,
-              }}
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "100px",
+        }}
+      >
+        {currentStep == 0 ? (
+          <>
+            <h1 style={{ marginBottom: "36px" }}>Sign-up</h1>
+            <div
               style={{
-                maxWidth: 600,
+                width: "640px",
+                border: "1px solid #e0e0e0",
+                borderRadius: "8px",
+                background: "#FFF",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                paddingTop: "24px",
               }}
-              initialValues={{
-                remember: true,
-              }}
-              autoComplete="off"
             >
-              <Form.Item
-                label="First name"
-                name="firstName"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your first name!",
-                  },
-                ]}
-              >
-                <Input value={firstName} onChange={onFirstNameChange} />
-              </Form.Item>
-
-              <Form.Item
-                label="Last name"
-                name="lastName"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input yourn last name!",
-                  },
-                ]}
-              >
-                <Input value={lastName} onChange={onLastNameChange} />
-              </Form.Item>
-
-              <Form.Item
-                label="Email"
-                name="email"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your Email!",
-                  },
-                  ({ getFieldValue }) => ({
-                    validator(rule, value) {
-                      if (
-                        value
-                          .toLowerCase()
-                          .match(
-                            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                          )
-                      ) {
-                        return Promise.resolve();
-                      }
-                      return Promise.reject("invalid email address");
-                    },
-                  }),
-                ]}
-              >
-                <Input value={email} onChange={onEmailChange} />
-              </Form.Item>
-              <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your password!",
-                  },
-                  ({ getFieldValue }) => ({
-                    validator(rule, value) {
-                      if (passwordRegx.test(password)) {
-                        return Promise.resolve();
-                      }
-                      return Promise.reject(
-                        "8-15 characters with lowercase, uppercase, numeric and special character(@ $ # ! % ? * &  ^)"
-                      );
-                    },
-                  }),
-                ]}
-              >
-                <Input.Password value={password} onChange={onPasswordChange} />
-              </Form.Item>
-
-              <Form.Item
-                label="Password confirm"
-                name="passwordConfirm"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please retype your password!",
-                  },
-                  ({ getFieldValue }) => ({
-                    validator(rule, value) {
-                      if (value == password) {
-                        return Promise.resolve();
-                      }
-                      return Promise.reject("please retype the same password");
-                    },
-                  }),
-                ]}
-              >
-                <Input.Password
-                  value={password2}
-                  onChange={onPassword2Change}
-                />
-              </Form.Item>
-
-              <Form.Item
-                name="errMsg"
+              <Form
+                name="basic"
+                labelCol={{
+                  span: 10,
+                }}
                 wrapperCol={{
-                  offset: 8,
                   span: 16,
                 }}
+                style={{
+                  maxWidth: 600,
+                }}
+                initialValues={{
+                  remember: true,
+                }}
+                autoComplete="off"
               >
-                <span style={{ color: "red" }}>{errMsg}</span>
-              </Form.Item>
+                <Form.Item
+                  label="First name"
+                  name="firstName"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your first name!",
+                    },
+                  ]}
+                >
+                  <Input value={firstName} onChange={onFirstNameChange} />
+                </Form.Item>
 
-              <Form.Item
-                wrapperCol={{
-                  offset: 11,
-                  span: 8,
+                <Form.Item
+                  label="Last name"
+                  name="lastName"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input yourn last name!",
+                    },
+                  ]}
+                >
+                  <Input value={lastName} onChange={onLastNameChange} />
+                </Form.Item>
+
+                <Form.Item
+                  label="Email"
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your Email!",
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(rule, value) {
+                        if (
+                          value
+                            .toLowerCase()
+                            .match(
+                              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                            )
+                        ) {
+                          return Promise.resolve();
+                        }
+                        return Promise.reject("invalid email address");
+                      },
+                    }),
+                  ]}
+                >
+                  <Input value={email} onChange={onEmailChange} />
+                </Form.Item>
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password!",
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(rule, value) {
+                        if (passwordRegx.test(password)) {
+                          return Promise.resolve();
+                        }
+                        return Promise.reject(
+                          "8-15 characters with lowercase, uppercase, numeric and special character(@ $ # ! % ? * &  ^)"
+                        );
+                      },
+                    }),
+                  ]}
+                >
+                  <Input.Password
+                    value={password}
+                    onChange={onPasswordChange}
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  label="Password confirm"
+                  name="passwordConfirm"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please retype your password!",
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(rule, value) {
+                        if (value == password) {
+                          return Promise.resolve();
+                        }
+                        return Promise.reject(
+                          "please retype the same password"
+                        );
+                      },
+                    }),
+                  ]}
+                >
+                  <Input.Password
+                    value={password2}
+                    onChange={onPassword2Change}
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  name="errMsg"
+                  wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                  }}
+                >
+                  <span style={{ color: "red" }}>{errMsg}</span>
+                </Form.Item>
+
+                <Form.Item
+                  wrapperCol={{
+                    offset: 11,
+                    span: 8,
+                  }}
+                >
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    onClick={onSubmit}
+                    loading={submitting}
+                  >
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+              <div
+                style={{
+                  display: "flex",
+                  color: "#757575",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  margin: "-12px 0 18px 0",
                 }}
               >
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  onClick={onSubmit}
-                  loading={submitting}
-                >
-                  Submit
+                Already have an account?
+                <Button type="link" onClick={goLogin}>
+                  Login
                 </Button>
-              </Form.Item>
-            </Form>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div style={{ display: "flex", flexDirection: "column" }}>
             <div
               style={{
                 display: "flex",
-                color: "#757575",
                 justifyContent: "center",
                 alignItems: "center",
-                margin: "-12px 0 18px 0",
+                height: "78px",
               }}
             >
-              Already have an account?
-              <Button type="link" onClick={goLogin}>
-                Login
+              <h3>Enter verification code for {email}</h3>
+            </div>
+            <OtpInput
+              value={otp}
+              onChange={onOTPchange}
+              numInputs={6}
+              shouldAutoFocus={true}
+              skipDefaultStyles={true}
+              inputStyle={{
+                height: "80px",
+                width: "60px",
+                border: "1px solid gray",
+                borderRadius: "6px",
+                textAlign: "center",
+                fontSize: "36px",
+              }}
+              renderSeparator={<span style={{ width: "36px" }}></span>}
+              renderInput={(props) => <input {...props} />}
+            />
+            <div
+              style={{
+                height: "64px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "red",
+              }}
+            >
+              {errMsg}
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+              }}
+            >
+              <Button
+                type="primary"
+                onClick={onSubmit2}
+                loading={submitting}
+                disabled={submitting}
+                size="large"
+              >
+                submit
+              </Button>
+              <Button
+                type="link"
+                block
+                onClick={onSubmit}
+                disabled={submitting}
+              >
+                Resend
               </Button>
             </div>
           </div>
-        </>
-      ) : (
-        <div style={{ display: "flex", flexDirection: "column" }}>
+        )}
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          bottom: "0",
+          height: "128px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          background: "#192733",
+          color: "#FFF",
+        }}
+      >
+        <div style={{ width: "80%" }}>
+          <Divider style={{ border: "#FFF", width: "80%" }}>
+            <div style={{ display: "flex", gap: "24px", color: "#FFF" }}>
+              <GithubOutlined style={{ fontSize: "24px" }} />
+              <TwitterOutlined style={{ fontSize: "24px" }} />
+              <LinkedinOutlined style={{ fontSize: "24px" }} />
+              <FacebookOutlined style={{ fontSize: "24px" }} />
+            </div>
+          </Divider>
           <div
             style={{
+              color: "#FFF",
               display: "flex",
-              justifyContent: "center",
               alignItems: "center",
-              height: "78px",
+              justifyContent: "center",
             }}
           >
-            <h3>Enter verification code for {email}</h3>
-          </div>
-          <OtpInput
-            value={otp}
-            onChange={onOTPchange}
-            numInputs={6}
-            shouldAutoFocus={true}
-            skipDefaultStyles={true}
-            inputStyle={{
-              height: "80px",
-              width: "60px",
-              border: "1px solid gray",
-              borderRadius: "6px",
-              textAlign: "center",
-              fontSize: "36px",
-            }}
-            renderSeparator={<span style={{ width: "36px" }}></span>}
-            renderInput={(props) => <input {...props} />}
-          />
-          <div
-            style={{
-              height: "64px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              color: "red",
-            }}
-          >
-            {errMsg}
-          </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Button
-              type="primary"
-              onClick={onSubmit2}
-              loading={submitting}
-              disabled={submitting}
-              size="large"
-            >
-              submit
-            </Button>
-            <Button type="link" block onClick={onSubmit} disabled={submitting}>
-              Resend
-            </Button>
+            Copyright © 2024 UniBee, Inc.
           </div>
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 };
 
