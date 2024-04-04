73788998876618286c2326fd34026bc60db15a54
Edward <edward.wo@wowow.io>
2024-04-04 11:12:08 +0800

    feat: 'buy addon' response

--- src/components/modals/onetimePaymentModal.tsx
+++ src/components/modals/onetimePaymentModal.tsx
@@ -66,12 +66,16 @@ const Index = ({ isOpen, closeModal, plan, subscriptionId }: Props) => {
       return;
     }
     console.log('payment res: ', paymentRes);
-    /*
-    if (paymentRes.link != null && paymentRes.link != '') {
-      window.open(paymentRes.link, '_blank');
+    if (paymentRes.paid) {
+      closeModal();
+      message.success('Payment succeeded');
+      return;
     }
-    */
-    return;
+
+    window.open(paymentRes.link, '_blank');
+    closeModal();
+    // Do I need to refresh the parent?
+    // Yes, if there are some unpaid fee, parent need to show it.
   };
 
   useEffect(() => {
@@ -110,57 +114,65 @@ const Index = ({ isOpen, closeModal, plan, subscriptionId }: Props) => {
       footer={null}
       closeIcon={null}
     >
-      <div className=" my-4 h-6">Are you sure you want to buy this addon?</div>
-      <Row style={{ height: '36px' }}>
-        <Col span={8}>
-          <span className=" font-bold text-gray-500">Addon name</span>
-        </Col>
-        <Col span={16}>{plan?.planName}</Col>
-      </Row>
-      <Row style={{ height: '36px' }}>
-        <Col span={8}>
-          <span className=" font-bold text-gray-500">Addon description</span>
-        </Col>
-        <Col span={16}>{plan?.description}</Col>
-      </Row>
-      <Row style={{ height: '36px' }}>
-        <Col span={8}>
-          <span className=" font-bold text-gray-500">Price</span>
-        </Col>
-        <Col span={16}>
-          {showAmount(plan?.amount as number, plan?.currency as string)}
-        </Col>
-      </Row>
-      <Row style={{ height: '36px' }}>
-        <Col span={8}>
-          <span className=" font-bold text-gray-500">Quantity</span>
-        </Col>
-        <Col span={16}>
-          <InputNumber
-            min={1}
-            max={100}
-            value={quantity}
-            onChange={onQuantityChange}
-          />
-        </Col>
-      </Row>
+      <div className="modal-content-wrapper">
+        <div>
+          <div className=" my-4 h-6">
+            Are you sure you want to buy this addon?
+          </div>
+          <Row style={{ height: '36px' }}>
+            <Col span={8}>
+              <span className=" font-bold text-gray-500">Addon name</span>
+            </Col>
+            <Col span={16}>{plan?.planName}</Col>
+          </Row>
+          <Row style={{ height: '36px' }}>
+            <Col span={8}>
+              <span className=" font-bold text-gray-500">
+                Addon description
+              </span>
+            </Col>
+            <Col span={16}>{plan?.description}</Col>
+          </Row>
+          <Row style={{ height: '36px' }}>
+            <Col span={8}>
+              <span className=" font-bold text-gray-500">Price</span>
+            </Col>
+            <Col span={16}>
+              {showAmount(plan?.amount as number, plan?.currency as string)}
+            </Col>
+          </Row>
+          <Row style={{ height: '36px' }}>
+            <Col span={8}>
+              <span className=" font-bold text-gray-500">Quantity</span>
+            </Col>
+            <Col span={16}>
+              <InputNumber
+                min={1}
+                max={100}
+                value={quantity}
+                onChange={onQuantityChange}
+              />
+            </Col>
+          </Row>
 
-      <div className=" my-4 w-3/4">
-        <PaymentSelector selected={gatewayId} onSelect={onGatewayChange} />
-      </div>
+          <div className=" my-4 w-3/4">
+            <PaymentSelector selected={gatewayId} onSelect={onGatewayChange} />
+          </div>
 
-      <div className="mt-6 flex items-center justify-end gap-4">
-        <Button onClick={closeModal} disabled={loading}>
-          Cancel
-        </Button>
-        <Button
-          type="primary"
-          onClick={onConfirm}
-          loading={loading}
-          disabled={loading}
-        >
-          OK
-        </Button>
+          <div className="mt-6 flex items-center justify-end gap-4">
+            <Button onClick={closeModal} disabled={loading}>
+              Cancel
+            </Button>
+            <Button
+              type="primary"
+              onClick={onConfirm}
+              loading={loading}
+              disabled={loading}
+            >
+              OK
+            </Button>
+          </div>
+        </div>
       </div>
     </Modal>
   );
